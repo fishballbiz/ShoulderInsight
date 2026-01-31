a162b263e22cacbc712c3efeffaa7c820f12689d
Andy Cheng <andy@foundi.info>
2026-01-31 08:59:43 +0800

    remove unused accounts app and collectstatic files

--- config/settings.py
+++ config/settings.py
@@ -69,7 +69,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = os.environ.get('STATIC_URL', 'static/')
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = '/tmp/staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.environ.get('MEDIA_ROOT', '/media')
